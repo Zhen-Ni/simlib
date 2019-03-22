c80462bf007a53f379c2b6b82299efc9f29ea4a6
Zhen NI <48048142+Zhen-Ni@users.noreply.github.com>
2019-03-22 18:05:24 +0800

    deal with previous commitment mistake

--- simlib/simlib/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from .simsys import *
-from .blocks import *
-from .tools import *
-
-__all__ = [s for s in dir() if not s.startswith('_')]
--- simlib/simlib/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-from .system_manipulation import *
-from .identification import *
-
-__all__ = [s for s in dir() if not s.startswith('_')]
