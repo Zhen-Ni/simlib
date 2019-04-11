788419bc7dd6f1d8c75b8e3f935eba460fc0806f
Zhen NI <48048142+Zhen-Ni@users.noreply.github.com>
2019-04-11 19:59:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup, Extension
 setup(name='simlib',
       version='in-dev',
       description='Python Simulation Library',
-      author='Zhen Ni',
+      author='Zhen NI',
       author_email='z.ni@hotmail.com',
       packages=['simlib', 'simlib.blocks', 'simlib.tools', 'simlib.blocks.special'],
       package_data={'simlib': ['simulinkplot.m']},
